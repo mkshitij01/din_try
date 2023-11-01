73ae829a2a30fccbbc0ecec7e1af72a5b6477f61
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-02 00:38:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,25 @@ with st.sidebar:
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-with st.sidebar:
-    selected = option_menu("Menu", ["Home", option_menu('Settings', ['link1', 'link2'])],
-        icons=['house', 'gear'], default_index=0)
+def home_button():
+    st.write("Home button pressed")
+    # Add your code for the Home button action here
 
-if selected == "Home":
-    st.write("home is where the heart is")
-    
-else:
-    st.write("settings is my bettings")
+def settings_button(selected_sub_option):
+    if selected_sub_option == "Rename":
+        st.write("Rename option selected")
+        # Add your code for the Rename action here
+    elif selected_sub_option == "Save":
+        st.write("Save option selected")
+        # Add your code for the Save action here
+
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+                           icons=['house', 'gear'], menu_icon="cast", default_index=1)
+
+    if selected == 'Settings':
+        selected_sub_option = st.selectbox("Sub-options", ["Rename", "Save"])
+        settings_button(selected_sub_option)
+
+    if st.button("Home"):
+        home_button()
