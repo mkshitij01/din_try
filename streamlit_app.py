8d35bac99b93838c19d4133f7d5a71415920eaef
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-02 00:35:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,11 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    selected = option_menu("Menu", ["Home", 'Settings'], 
+    selected = option_menu("Menu", ["Home", option_menu('Settings', ['link1', 'link2'], icons=['house', 'gear'], default_index=0), 
         icons=['house', 'gear'], default_index=0)
 
 if selected == "Home":
     st.write("home is where the heart is")
+    
 else:
     st.write("settings is my bettings")
