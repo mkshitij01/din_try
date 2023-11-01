16667bf3a8fa0e649452aa2bc8615f782d8704b4
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-02 00:58:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,31 @@ if st.sidebar.button("Section 1"):
 if st.sidebar.button("Section 2"):
     st.write("You are in Section 2")
 
-# Second-level buttons with indentation
+# Second-level buttons with increased margin for indentation
 with st.sidebar:
     st.write("   ")  # Add some padding for indentation
+    st.write(
+        """
+        <style>
+        .stButton:nth-child(3) {
+            margin-top: 20px;
+        }
+        </style>
+        """
+    )
     if st.button("Subsection 1"):
         st.write("You are in Subsection 1")
 
 with st.sidebar:
     st.write("   ")  # Add some padding for indentation
+    st.write(
+        """
+        <style>
+        .stButton:nth-child(5) {
+            margin-top: 20px;
+        }
+        </style>
+        """
+    )
     if st.button("Subsection 2"):
         st.write("You are in Subsection 2")
