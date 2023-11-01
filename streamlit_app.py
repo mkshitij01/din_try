ed4cebe6fcb7258862f992735581fa7675d9df22
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-02 00:19:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+'''
 import streamlit as st
 from streamlit_option_menu import option_menu
 
@@ -5,3 +6,24 @@ with st.sidebar:
     selected = option_menu("Main Menu", ["Home", 'Settings'], 
         icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
+'''
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+def home_button():
+    st.write("Home button pressed")
+    # Add your code for the Home button action here
+
+def settings_button():
+    st.write("Settings button pressed")
+    # Add your code for the Settings button action here
+
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+                           icons=['house', 'gear'], menu_icon="cast", default_index=1)
+
+    if st.button("Home"):
+        home_button()
+
+    if st.button("Settings"):
+        settings_button()
