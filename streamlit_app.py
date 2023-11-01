9013ca369cc34ad98ae2524cc3a35c34524ca6d4
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-01 22:50:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,19 @@
 import streamlit as st
 
-# Create a dictionary to represent the navigation structure
-navigation = {
-    "Level 1 Link 1": {
-        "Sublink 1": "/sublink1",
-        "Sublink 2": "/sublink2",
-    },
-    "Level 1 Link 2": {
-        "Sublink 3": "/sublink3",
-    },
-    "Level 1 Link 3": {
-        "Sublink 4": "/sublink4",
-        "Sublink 5": "/sublink5",
-    },
-}
+# Create the left-side navigation bar with hyperlinks
+st.sidebar.markdown('[Home](#)')
+st.sidebar.markdown('[Link1](#)')
+st.sidebar.markdown('[Link2](#)')
 
-# Create the navigation bar
-st.sidebar.title("Navigation")
+# Create a dropdown for "Drop1" with hyperlinks
+with st.sidebar.beta_expander("Drop1"):
+    st.sidebar.markdown('[Link3](#)')
+    st.sidebar.markdown('[Link4](#)')
 
-# Iterate through the Level 1 links and their sublinks
-for level1_link, sublinks in navigation.items():
-    st.sidebar.subheader(level1_link)
+# Create "Link4" outside the dropdown
+st.sidebar.markdown('[Link4](#)')
 
-    # Create buttons for sublinks under the Level 1 link
-    for sublink, link in sublinks.items():
-        if st.sidebar.button(sublink):
-            st.experimental_set_query_params(**{"page": link})
-
-# Display content based on the selected link
-if "page" in st.experimental_get_query_params():
-    selected_page = st.experimental_get_query_params()["page"]
-    st.write(f"Content for the selected page: {selected_page}")
+# Create a dropdown for "Drop2" with hyperlinks
+with st.sidebar.beta_expander("Drop2"):
+    st.sidebar.markdown('[Link5](#)')
+    st.sidebar.markdown('[Link6](#)')
