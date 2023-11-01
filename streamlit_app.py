24178cb4c6da0616bd2688d6002cce8417c41572
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-02 01:03:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,25 @@
 import streamlit as st
 
-# Sidebar navigation items
-sidebar_items = {
-    "Level 1 Item 1": ["Level 2 Item 1", "Level 2 Item 2"],
-    "Level 1 Item 2": [],  # Empty list for level 2
-}
+# Define functions for each link action
+def home_action():
+    st.write("You clicked on Home!")
 
-# Sidebar title
-st.sidebar.title("Sidebar Navigation")
+def edit_action():
+    st.write("You clicked on Edit!")
 
-# Level 1 selection
-level1 = st.sidebar.selectbox("Select Level 1", list(sidebar_items.keys()))
+def rename_action():
+    st.write("You clicked on Rename!")
 
-# Level 2 selection (if level 1 item has sub-items)
-level2 = st.sidebar.selectbox("Select Level 2", sidebar_items.get(level1, []))
+def about_action():
+    st.write("You clicked on About!")
 
-# Content based on selections
-st.write(f"Selected Level 1: {level1}")
-st.write(f"Selected Level 2: {level2}")
+# Create clickable links in the sidebar
+if st.sidebar.button("Home"):
+    home_action()
+if st.sidebar.button("Settings"):
+    if st.sidebar.button("Edit"):
+        edit_action()
+    if st.sidebar.button("Rename"):
+        rename_action()
+if st.sidebar.button("About"):
+    about_action()
