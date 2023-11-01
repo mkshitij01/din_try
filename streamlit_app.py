f79cc6d4d66559e2f4ae978476a3f46d8b6a478c
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-01 23:04:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,7 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-# Create a sidebar
-st.sidebar.title("Sidebar")
-
-# Define the links and dropdowns
-links = {
-    "About": "#about",
-    "Services": "#services",
-    "Clients": "#clients",
-    "Contact": "#contact",
-}
-
-# Dropdown for "Dropdown"
-dropdown_links = {
-    "Link 1": "#link1",
-    "Link 2": "#link2",
-    "Link 3": "#link3",
-}
-
-# Display the links
-for text, link in links.items():
-    st.sidebar.markdown(f"[{text}]({link})")
-
-# Display the dropdown
-with st.sidebar.expander("Dropdown"):
-    for text, link in dropdown_links.items():
-        st.sidebar.markdown(f"[{text}]({link})")
-
-# Main content
-st.title("Main Content")
-st.write("This is the main content area.")
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
