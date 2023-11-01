15711a9fa56762dd932158ef879889c858918915
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-02 01:05:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,19 +13,15 @@ def rename_action():
 def about_action():
     st.write("You clicked on About!")
 
-# Define a CSS style for consistent button size
-button_style = "padding: 10px; width: 200px; text-align: left;"
+# Create clickable links in the sidebar
+if st.sidebar.button("Home"):
+    home_action()
 
-# Create clickable links in the sidebar with consistent size
-if st.sidebar.button("Home", key="home", help="Home Button", type="default", on_click=home_action, args=()):
-    pass
+if st.sidebar.button("Settings"):
+    if st.sidebar.button("Edit"):
+        edit_action()
+    if st.sidebar.button("Rename"):
+        rename_action()
 
-if st.sidebar.button("Settings", key="settings", help="Settings Button", type="default", on_click=None, args=()):
-    if st.sidebar.button("Edit", key="edit", help="Edit Button", type="default", on_click=edit_action, args=()):
-        pass
-
-    if st.sidebar.button("Rename", key="rename", help="Rename Button", type="default", on_click=rename_action, args=()):
-        pass
-
-if st.sidebar.button("About", key="about", help="About Button", type="default", on_click=about_action, args=()):
-    pass
+if st.sidebar.button("About"):
+    about_action()
