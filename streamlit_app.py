0f46f1551edf63cc6928507334ec3182416276b3
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-02 00:59:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,24 @@ import streamlit as st
 # Create a sidebar navigation bar
 st.sidebar.title("Sidebar Navigation")
 
+# Define the CSS for styling the buttons
+button_css = """
+<style>
+.stButton {
+    width: 100%;
+    text-align: left;
+    padding-left: 30px;
+    margin-bottom: 10px;
+}
+
+.stButton.subsection {
+    margin-left: 20px;
+}
+</style>
+"""
+
+st.sidebar.markdown(button_css, unsafe_allow_html=True)
+
 # First-level buttons
 if st.sidebar.button("Section 1"):
     st.write("You are in Section 1")
@@ -11,29 +29,9 @@ if st.sidebar.button("Section 2"):
 
 # Second-level buttons with increased margin for indentation
 with st.sidebar:
-    st.write("   ")  # Add some padding for indentation
-    st.write(
-        """
-        <style>
-        .stButton:nth-child(3) {
-            margin-top: 20px;
-        }
-        </style>
-        """
-    )
-    if st.button("Subsection 1"):
+    if st.button("Subsection 1", key="subsection1", class="subsection"):
         st.write("You are in Subsection 1")
 
 with st.sidebar:
-    st.write("   ")  # Add some padding for indentation
-    st.write(
-        """
-        <style>
-        .stButton:nth-child(5) {
-            margin-top: 20px;
-        }
-        </style>
-        """
-    )
-    if st.button("Subsection 2"):
+    if st.button("Subsection 2", key="subsection2", class="subsection"):
         st.write("You are in Subsection 2")
