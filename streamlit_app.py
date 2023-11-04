0c19338c39e7869804b3fce14f9e4c3afc1297f7
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-04 05:46:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,6 @@ with st.sidebar:
                 sac.MenuItem('Edit', icon='git'),
             ]),
         ]),
-        '''
-        sac.MenuItem(type='divider'),
-        sac.MenuItem('reference', type='group', children=[
-            sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
-            sac.MenuItem('bootstrap-icon', icon='bootstrap', href='https://icons.getbootstrap.com/'),
-        ]),
-        '''
     ], format_func='title', open_all=True)
 
 if Item_menu == 'home':
