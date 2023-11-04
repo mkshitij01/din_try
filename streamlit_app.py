3a544ce23537805c1343df81f4b574ec360b373b
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-04 05:44:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,20 @@ import Home
 with st.sidebar:
     
     Item_menu = sac.menu([
-        sac.MenuItem('home', icon='house-fill'),
-        sac.MenuItem('products', icon='box-fill', children=[
-            sac.MenuItem('apple', icon='apple', tag=sac.Tag('USA', color='green', bordered=False)),
-            sac.MenuItem('other', icon='git'),
+        sac.MenuItem('Home', icon='house-fill'),
+        sac.MenuItem('link1', icon='send'),
+        sac.MenuItem('link2', icon='send',),
+        sac.MenuItem('Admin', icon='box-fill', children=[
+            sac.MenuItem('Mappings', icon='apple', children=[
+                sac.MenuItem('Create', icon='git'),
+                sac.MenuItem('Edit', icon='git'),
+            ]),
         ]),
-        sac.MenuItem('disabled', icon='send', disabled=True),
+        '''
         sac.MenuItem(type='divider'),
         sac.MenuItem('reference', type='group', children=[
             sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
-            sac.MenuItem('bootstrap-icon', icon='bootstrap', href='https://icons.getbootstrap.com/'),
+            sac.MenuItem('bootstrap-icon', icon='bootstrap', href='https://icons.getbootstrap.com/'),'''
         ]),
     ], format_func='title', open_all=True)
 
