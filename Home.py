df9105d1bd833c893dc6875be5c843ee4552cabc
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-06 05:50:37 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,6 +6,18 @@ def app():
     st.title('Home')
     st.write('This is the `home page` of this multi-page app.')
     st.write('In this app, we will be building a simple classification model using the Iris dataset.')
-    
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.header('Bar Chart')
+        chart_data = pd.DataFrame(np.random.randn(10, 3), columns=["a", "b", "c"])
+        st.bar_chart(chart_data)
+
+    with col2:
+        st.header('Bar Chart')
+        chart_data = pd.DataFrame(np.random.randn(10, 3), columns=["a", "b", "c"])
+        st.bar_chart(chart_data)
+
     chart_data = pd.DataFrame(np.random.randn(10, 3), columns=["a", "b", "c"])
     st.bar_chart(chart_data)
