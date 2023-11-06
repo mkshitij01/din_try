5fd990059cc9d30e82252069f98d87554cc4eb86
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-06 05:44:23 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 def app():
     st.title('Home')
     st.write('This is the `home page` of this multi-page app.')
     st.write('In this app, we will be building a simple classification model using the Iris dataset.')
+    
+    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+    st.bar_chart(chart_data)
