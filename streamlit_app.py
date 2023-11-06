590400a4b1ff2477d0292d62da9a7d9a05359e2f
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-06 06:20:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import streamlit_antd_components as sac
 import Home
+import schedules
 
 with st.sidebar:
     
     Item_menu = sac.menu([
         sac.MenuItem('Home', icon='house-fill'),
-        sac.MenuItem('link1', icon='send'),
+        sac.MenuItem('Schedules', icon='send'),
         sac.MenuItem('link2', icon='send',),
         sac.MenuItem('Admin', icon='box-fill', children=[
             sac.MenuItem('Mappings', icon='box-fill', children=[
@@ -18,5 +19,7 @@ with st.sidebar:
 
 if Item_menu == 'Home':
     Home.app()
-elif Item_menu == 'link1':
-    st.write('yayaya')
+elif Item_menu == 'Schedules':
+    schedules.app()
+elif Item_menu == 'link2':
+    st.write('Link 2 was clicked!')
