a942b44cd533d4a2f7612fddce72ce33353dd5e8
mkshitij01 <149021958+mkshitij01@users.noreply.github.com>
2023-11-06 05:51:56 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,9 +15,9 @@ def app():
         st.bar_chart(chart_data)
 
     with col2:
-        st.header('Bar Chart')
+        st.header('Line Chart')
         chart_data = pd.DataFrame(np.random.randn(10, 3), columns=["a", "b", "c"])
-        st.bar_chart(chart_data)
+        st.line_chart(chart_data)
 
     chart_data = pd.DataFrame(np.random.randn(10, 3), columns=["a", "b", "c"])
     st.bar_chart(chart_data)
